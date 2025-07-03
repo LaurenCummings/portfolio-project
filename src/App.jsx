49a9e2f90bdb11c397fbe601374f1d840849adce
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-02 23:28:39 -0400

    added spa project route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './css/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Topbar from './components/Topbar';
 import Home from './pages/Home';
+import SpaProject from './pages/SpaProject';
 import ShopProject from './pages/ShopProject';
 import MovieProject from './pages/MovieProject';
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/portfolio-project" element={<Home />} />
+        <Route path="/spa-project" element={<SpaProject />} />
         <Route path="/shop-project" element={<ShopProject />} />
         <Route path="/movie-project" element={<MovieProject />} />
       </Routes>
