00557777c58925a149a45139838f5e53d0a6590c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-02 23:36:04 -0400

    updated content of CummingsMaple project page

--- src/pages/CummingsMaple.jsx
+++ src/pages/CummingsMaple.jsx
@@ -6,18 +6,15 @@ function CummingsMaple() {
     return (
         <div className="selected-project">
             <div className="project-info">
-                <a href="https://laurencummings.github.io/spa-project/" >
+                <a href="https://cummingsmaple.com" >
                     <img src={movieCollage} alt="Collage of movie project" />
                 </a>
                 <div className="project-details">
                     <ul>
-                        <li>A mock business site for a spa, where users can read about services offered and schedule appointments</li>
+                        <li>A site for a local business where users can view products and read about the company's history</li>
                         <li>Built with React</li>
-                        <li>A single page design with no routing</li>
-                        <li>Features auto-scrolling to navigate page</li>
-                        <li>Utilizes React-Calendar for date selection</li>
-                        <li>CSS animations used to create a continuously moving banner</li>
-                        <li>View the code on <a href="https://github.com/LaurenCummings/spa-project">Github</a></li>
+                        <li>Responsive down to XXX px</li>
+                        <li>View the code on <a href="https://github.com/LaurenCummings/cummings-maple-vite">Github</a></li>
                     </ul>
                 </div>
             </div>
