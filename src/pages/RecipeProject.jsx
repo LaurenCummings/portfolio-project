5239ebe304cff4d14214a24a6f5a7cb6b78b8ee6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-13 00:35:57 -0400

    added content to RecipePage

--- src/pages/RecipeProject.jsx
+++ src/pages/RecipeProject.jsx
@@ -1,7 +1,24 @@
+import '../css/Project.css';
+
 function RecipeProject() {
     return (
-        <div>
-
+        <div className="selected-project">
+            <div className="project-info">
+                <a href="https://laurencummings.github.io/recipe-project2/" >
+                    <img src={cummingsMapleCollage} alt="Collage of Cummings Maple" />
+                </a>
+                <div className="project-details">
+                    <ul>
+                        <li>Data from DummyJSON API displayed in a usable format</li>
+                        <li>Built with React</li>
+                        <li>User can filter data by category</li>
+                        <li>Filter design changes to a hidable sidebar on smaller screens</li>
+                        <li>Recipe rating data is displayed with an intuitive 5-star element</li>
+                        <li>View the code on <a href="https://github.com/LaurenCummings/recipe-project2">Github</a></li>
+                    </ul>
+                </div>
+            </div>
+            
         </div>
     )
 }
