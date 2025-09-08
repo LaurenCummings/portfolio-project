3643ac8b522cc195a66eef5376cf5873cf721523
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-08 03:12:29 -0400

    gave demo blog a header

--- src/pages/BlogProject.jsx
+++ src/pages/BlogProject.jsx
@@ -1,28 +1,36 @@
-import '../css/Project.css';
-import blogCollage from '../assets/blogCollage.png';
+import "../css/Project.css";
+import blogCollage from "../assets/blogCollage.png";
 
 function BlogProject() {
-
-    return (
-        <div className="selected-project">
-            <div className="project-info">
-                <a href="https://laurencummings.github.io/blog-project/" >
-                    <img src={blogCollage} alt="Collage of blog project" />
-                </a>
-                <div className="project-details">
-                    <ul>
-                        <li>A demo blog created with the DummyJSON API</li>
-                        <li>Built with React</li>
-                        <li>Users can navigate through posts or search by keyword</li>
-                        <li>A shared "feed" area that switches control between blog posts and search results</li>
-                        <li>Responsive down to 350px</li>
-                        <li>View the code on <a href="https://github.com/LaurenCummings/blog-project">Github</a></li>
-                    </ul>
-                </div>
-            </div>
-            
+  return (
+    <div className="selected-project">
+      <h1>Demo Blog Project</h1>
+      <div className="project-info">
+        <a href="https://laurencummings.github.io/blog-project/">
+          <img src={blogCollage} alt="Collage of blog project" />
+        </a>
+        <div className="project-details">
+          <ul>
+            <li>A demo blog created with the DummyJSON API</li>
+            <li>Fetches data from an API</li>
+            <li>Built with React</li>
+            <li>Users can navigate through posts or search by keyword</li>
+            <li>
+              A shared "feed" area that switches control between blog posts and
+              search results
+            </li>
+            <li>Responsive down to 350px</li>
+            <li>
+              View the code on{" "}
+              <a href="https://github.com/LaurenCummings/blog-project">
+                Github
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default BlogProject;
