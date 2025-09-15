213b5387018403b8f08dfb418cceea091898b2d6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-15 13:09:03 -0400

    added scaffolding for cookbook and notes projects

---
+++ src/pages/NotesProject.jsx
@@ -0,0 +1,35 @@
+import "../css/Project.css";
+import blogCollage from "../assets/blogCollage.png";
+
+function NotesProject() {
+  return (
+    <div className="selected-project">
+      <h1>Demo Blog Project</h1>
+      <div className="project-info">
+        <a href="https://laurencummings.github.io/blog-project/">
+          <img src={blogCollage} alt="Collage of blog project" />
+        </a>
+        <div className="project-details">
+          <ul>
+            <li>Fetches data from an API</li>
+            <li>Built with React</li>
+            <li>Users can navigate through posts or search by keyword</li>
+            <li>
+              A shared "feed" area that switches control between blog posts and
+              search results
+            </li>
+            <li>Responsive down to 350px</li>
+            <li>
+              View the code on{" "}
+              <a href="https://github.com/LaurenCummings/blog-project">
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotesProject;
