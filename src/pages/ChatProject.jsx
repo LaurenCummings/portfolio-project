e90a230605d73735c72c117056bc42d50a824622
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-04 01:10:00 -0400

    added chat project page

---
+++ src/pages/ChatProject.jsx
@@ -0,0 +1,35 @@
+import "../css/Project.css";
+import cookbookProjectCollage from "../assets/cookbookProjectCollage.png";
+
+function ChatProject() {
+  return (
+    <div className="selected-project">
+      <h1>Personal Cookbook</h1>
+      <div className="project-info">
+        <a href="https://chat-project-rkax.onrender.com">
+          <img src={cookbookProjectCollage} alt="Collage of cookbook project" />
+        </a>
+        <div className="project-details">
+          <ul>
+            <li>
+              A personal project created to organize recipes I've collected
+            </li>
+            <li>Built with the MERN stack</li>
+            <li>Backend code features an API and NoSQL model for recipes</li>
+            <li>Logging in allows user to add, delete, and update recipes</li>
+            <li>Optional light/dark mode</li>
+            <li>Built with Chakra UI</li>
+            <li>
+              View the code on{" "}
+              <a href="https://github.com/LaurenCummings/cookbook-project">
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChatProject;
